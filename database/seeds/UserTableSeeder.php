86d21a6183b2a447f20515d0d3d5417a512d1fa2
DipuHaider <dipuhaider@gmail.com>
2019-02-03 07:20:35 +0600

    Update UserTableSeeder.php
    Using The model factory to generate ten users in run method.

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,5 +1,5 @@
 <?php
-
+use app/User;
 use Illuminate\Database\Seeder;
 
 class UserTableSeeder extends Seeder
@@ -11,6 +11,6 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+       $users = factory(User::class, 10)->create();
     }
 }
