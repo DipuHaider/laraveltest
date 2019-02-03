9ed3381d5d2fbd1d3f4f8195095974c0afedeaa9
DipuHaider <dipuhaider@gmail.com>
2019-02-03 08:48:53 +0600

    Update RouteController.php

--- app/Http/Controllers/RouteController.php
+++ app/Http/Controllers/RouteController.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Http\Requests;
 use Illuminate\Http\Request;
 
 class RouteController extends Controller
 {
-    //
+    public function index(){
+    return view(''helloworld);
+    }
 }
