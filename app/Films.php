ae8de56b9394e0002590a27df6dec5a9a1e5293e
DipuHaider <dipuhaider@gmail.com>
2019-02-03 07:58:16 +0600

    Update Films.php Model

--- app/Films.php
+++ app/Films.php
@@ -3,14 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Films extends Model
 {
     use Notifiable;
     protected $fillable = [
-        'name', 'description', 'release_date','rating', 'ticket_price', 'country','genre', 'photo',
+        'name', 'description', 'release_date','rating', 'ticket_price', 'country','genre', 'photo'
     ];
 }
